f8ce3f670fe19f24f0459c7d15895fd61d4264e1
MochDedy <mochdedy96@gmail.com>
2026-02-26 20:26:47 +0700

    Percobaan 2

---
+++ Pertemuan2/Mahasiswa14.java
@@ -0,0 +1,49 @@
+
+public class Mahasiswa14 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    public Mahasiswa14() {
+
+    }
+
+    public Mahasiswa14(String nm, String nim, String kls, double ipk) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid harus antara 0.0 dan 4.0");
+        }
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja Baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja Cukup";
+        } else {
+            return "Kinerja Kurang";
+        }
+    }
+}
